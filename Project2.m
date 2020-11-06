5cbb85f54f767f47fc9c30356813befe4b059084
Padexin <61795960+Padexin@users.noreply.github.com>
2020-11-06 08:12:13 -0700

    Add files via upload

---
+++ Project2.m
@@ -0,0 +1,54 @@
+%%Housekeeping
+clear all
+clc
+close all
+
+%%First, we will look at the age, female population, and female size 
+data1 = [0, 495, 30.3;
+        1,  92, 45.8;
+        2,  11, 55.8;
+        3,   2, 56.0];
+
+clutch_size = 0.21*data1(:,3) - 7.5; %Get our clutch size from the given formula
+clutch_size(1) = 0; %Hatchlings can't lay eggs
+
+survival = data1(2:end, 2)./data1(1:end-1,2); %Survival rate is estimated from the table
+
+%%Now we consider patch size, sandy area, F_a, S_j, S_a, and density of
+%%population
+
+data2 = [ 11.31,  4.80,  5.6, 0.12, 0.06,  58;
+          35.54, 11.31,  6.6, 0.16, 0.10,  60;
+         141.76, 51.55,  9.5, 0.17, 0.13,  75;
+          14.65,  7.55,  4.8, 0.15, 0.09,  55;
+          63.24, 20.12,  9.7, 0.17, 0.11,  80;
+         132.35, 54.14,  9.9, 0.18, 0.14,  82;
+           8.46,  1.67,  5.5, 0.11, 0.05,  40;
+         278.26, 84.32, 11.0, 0.19, 0.15, 115];
+     
+totals = data2(:,1).*data2(:,6); %total populations in each patch = size*density
+
+
+%We find the betas for F_a, S_j, S_a, and C via linear regression
+%First, a look at the data vs. patch size and sandy area
+scatter(data2(:,1) + data2(:,2), data2(:,3))
+figure()
+scatter(data2(:,1) + data2(:,2), data2(:,4))
+figure()
+scatter(data2(:,1) + data2(:,2), data2(:,5))
+figure()
+scatter(data2(:,1) + data2(:,2), totals)
+% beta_F = data2(:,3)\[ones(8,1),data2(:,1:2)]; 
+% beta_S_j = data2(:,4)\[ones(8,1),data2(:,1:2)];
+% beta_S_a = data2(:,5)\[ones(8,1),data2(:,1:2)];
+% beta_C = totals\[ones(8,1),data2(:,1:2)];
+
+
+%%Next we will look at migration between patches. Specifically, we will
+%%look at distance moved and probability of recapture
+data3 = [ 50, 0.42;
+         100, 0.25;
+         200, 0.12;
+         250, 0.02;
+         300, 0.00;
+         350, 0.01];
